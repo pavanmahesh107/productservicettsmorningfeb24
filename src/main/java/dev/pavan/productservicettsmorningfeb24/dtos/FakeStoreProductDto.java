f26d2661916fc2800430db31b5c6513358642bf0
pavanmahesh1518 <159711431+pavanmahesh1518@users.noreply.github.com>
2024-03-01 14:57:10 +0530

    post and get mapping

---
+++ src/main/java/dev/pavan/productservicettsmorningfeb24/dtos/FakeStoreProductDto.java
@@ -0,0 +1,34 @@
+package dev.pavan.productservicettsmorningfeb24.dtos;
+
+
+import dev.pavan.productservicettsmorningfeb24.models.Category;
+import dev.pavan.productservicettsmorningfeb24.models.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FakeStoreProductDto {
+    private Long id;
+    private String title;
+    private String image;
+    private String description;
+    private String category;
+    private double price;
+
+    public Product toProduct(){
+        Product product = new Product();
+        product.setId(id);
+        product.setTitle(title);
+        product.setImageUrl(image);
+        product.setDescription(description);
+        product.setPrice(price);
+
+        Category productCategory  = new Category();
+        productCategory.setTitle(category);
+
+        product.setCategory(productCategory);
+
+        return product;
+    }
+}
